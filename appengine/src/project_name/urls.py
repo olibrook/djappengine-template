fbb5ef443f1e4fd2fce40ac77e4e97a5948e9f0b
Oliver Brook <olibrook@gmail.com>
2014-03-31 13:40:51 +0100

    Stupid Hello World view.

--- appengine/src/project_name/urls.py
+++ appengine/src/project_name/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django import http
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,5 +18,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^$', lambda request: http.HttpResponse('Hello world!')),
     url(r'^djappengine/sessions/', include(djappengine.sessions.urls)),
 )
